19b3f36dc3ae468207cac93e03e18cc6ba19ab27
user <email@email.email>
2016-06-15 09:50:03 -0300

    Subida 2

---
+++ blog/blog.py
@@ -0,0 +1,38 @@
+# -*- coding: cp1252 -*-
+print "-------------------------BLOG-----------------------------------"
+print "Login"
+while True:
+    usu = raw_input("Usuario:")
+    clave = raw_input("Contraseña:")
+
+
+    if (usu == "Franco.G" and clave == "123qwe") or (usu == "Carlos:M" and clave == "123123123") or (usu == "Bastian.S" and clave == "qweqweqwe") or (usu == "Jonathan.V" and clave == "hola123456789")(usu == "admin" and clave == "admin"):
+        print "Bienvenido ", usu
+        break
+
+
+
+while True:
+    print "1-. Ecribir comentario ","2-. Responder comentario","3-. Me gusta","4-. No me gusta"
+    if (usu == "admin"and clave =="admin"):
+        print "5-. Borrar comentarios"
+
+    op = input("Ingrese opcion: ")
+    if op ==1:
+        cont = 0
+        lim = 50
+        print "Comentario: "
+        print
+
+    elif op == 2:
+        break
+    elif op == 3:
+        cont1 = 0
+
+
+
+        cont1 += 1
+    elif op ==4:
+
+        cont1 == cont1 -1
+
